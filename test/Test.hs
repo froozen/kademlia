ffd2f0abcf3874dcd9ab93f44f55681495bdd12f
fro_ozen <fro_ozen@gmx.de>
2015-03-29 22:45:45 +0200

    Use the Tasty testing framework
    Wohoo, nice colored output!

--- test/Test.hs
+++ test/Test.hs
@@ -2,33 +2,25 @@
 Module      : Tests
 Description : Tests for the modules
 
-A few tests using QuickCheck to make sure everything works
+A few tests using QuickCheck and Tasty to make sure everything works
 the way it's supposed to.
 -}
 
-module Test where
+module Main where
+
+import Test.Tasty
+import Test.Tasty.QuickCheck as QC
 
 import Protocol
 import Networking
 
-import Test.QuickCheck as Q
-import Distribution.TestSuite as TS
+main = defaultMain tests
 
-toTSResult :: Q.Result -> TS.Result
-toTSResult Q.Success {} = TS.Pass
-toTSResult Q.GaveUp {} = TS.Fail "Gave up"
-toTSResult Q.Failure {reason=qReason} = TS.Fail qReason
+tests :: TestTree
+tests = testGroup "Tests" [quickCheckTests]
 
-runQuickCheck :: Q.Testable p => p -> IO TS.Progress
-runQuickCheck prop = do
-    qres <- Q.quickCheckWithResult Q.stdArgs prop
-    return $ Finished . toTSResult $ qres
-
-tests :: IO [Test]
-tests = return [ Test $ TestInstance (runQuickCheck parseCheck)
-                                      "parseCheck" [] [] undefined
-               , Test $ TestInstance (runQuickCheck sendCheck)
-                                      "sendCheck" [] [] undefined
-               , Test $ TestInstance (runQuickCheck lengthCheck)
-                                      "lengthCheck" [] [] undefined
-               ]
+quickCheckTests = testGroup "QuickCheck Tests" [
+      QC.testProperty "parseCheck" parseCheck
+    , QC.testProperty "sendCheck" sendCheck
+    , QC.testProperty "lengthCheck" lengthCheck
+    ]
