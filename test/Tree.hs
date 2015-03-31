b2d0f23f3723b3fb0d4d77a746d560bd168bc8e6
fro_ozen <fro_ozen@gmx.de>
2015-03-31 11:56:56 +0200

    Add lookupCheck to test/Tree
    This makes the other tests simpler.

--- test/Tree.hs
+++ test/Tree.hs
@@ -14,13 +14,17 @@ import Network.Kademlia.Types
 
 import TestTypes
 
+-- | Helper method for lookup checking
+lookupCheck :: (Serialize i, Eq i) => T.NodeTree i -> Node i -> Bool
+lookupCheck tree node = T.lookup tree (nodeId node) == Just node
+
 -- | Check wether an inserted Node is retrievable
 insertCheck :: IdType -> Node IdType -> Bool
-insertCheck id node = T.lookup tree (nodeId node) == Just node
+insertCheck id node = lookupCheck tree node
     where tree = T.insert (T.create id) node
 
 -- | Make sure a deleted Node can't be retrieved anymore
 deleteCheck :: IdType -> Node IdType -> Bool
-deleteCheck id node = T.lookup tree id == Nothing
+deleteCheck id node = not . lookupCheck tree $ node
     where tree = T.delete origin . nodeId $ node
           origin = T.insert (T.create id) node
