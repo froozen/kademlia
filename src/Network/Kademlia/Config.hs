3d37f9d106976f6105f9e76012d7d52cae2ad5da
Vincent Hanquez <vincent.hanquez@iohk.io>
2016-12-07 20:54:19 +0000

    add configuration for number of lookup nodes in parallel

--- src/Network/Kademlia/Config.hs
+++ src/Network/Kademlia/Config.hs
@@ -9,10 +9,13 @@ data KademliaConfig = KademliaConfig {
       expirationTime :: Int -- in seconds
     , storeValueTime :: Int -- in seconds
     , pingTime       :: Int -- in seconds
+    , nbLookupNodes  :: Int -- number of nodes to look in parallel during a lookup
+                            -- also known as α in kademlia paper
     }
 
 defaultConfig = KademliaConfig
     { expirationTime = hour 1
     , storeValueTime = hour 1
     , pingTime       = minute 5
+    , nbLookupNodes  = 3
     }
